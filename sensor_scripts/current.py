0fe2eb1d5d25bc573c163ac3016df39a793e5a82
Daniele Venturino <venturino.daniele@gmail.com>
2016-08-09 17:13:22 +0200

    Convert the atmospheric pressure to sea-level pressure value.
    There is now a parameter in both current.py and logger.py
    script to set the measurements altitude.
    
    Formula from: http://keisan.casio.com/exec/system/1224575267

--- sensor_scripts/current.py
+++ sensor_scripts/current.py
@@ -4,11 +4,17 @@ from Adafruit_BME280 import *
 
 sensor = BME280(mode=BME280_OSAMPLE_8)
 
+# Altitude in meters to calculate sea-level pressure
+altitude = 93
+
 try:
     temperature = sensor.read_temperature()
     humidity = sensor.read_humidity()
     pascals = sensor.read_pressure()
     hectopascals = pascals / 100
+    # Adjust pressure to sea level
+    hectopascals = hectopascals*(1-(0.0065 * altitude)/(temperature + 0.0065 * altitude + 273.15))**(-5.257)
+
     print '{0:0.1f}\n{1}\n{2:0.2f}'.format(temperature, int(humidity), hectopascals)
 except RuntimeError as e:
     print 'error\n{0}'.format(e)
